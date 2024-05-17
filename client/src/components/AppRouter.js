a1a7761c9f70536ea35f3ef50b0d6cfaa51b2855
Rodion Yermakov <rodion.yermakov.sd@gmail.com>
2024-05-16 17:40:44 -0700

    mobx and user store and device store

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { authRoutes, publicRoutes } from '../routes';
-import { SHOP_ROUTE } from '../utils/consts';
+import { Context } from '../index';
 
 const AppRouter = () => {
     const isAuth = false;
 
+    const { user } = useContext(Context);
+
+    console.log(user);
+
     return (
         <>
             <Routes>
-                {isAuth &&
+                {user.isAuth &&
                     authRoutes.map(({ path, Component }) => (
                         <Route key={path} path={path} element={<Component />} exact />
                     ))}
                 {publicRoutes.map(({ path, Component }) => (
                     <Route key={path} path={path} element={<Component />} exact />
                 ))}
-                {/* <Navigate to='/' /> */}
             </Routes>
         </>
     );
